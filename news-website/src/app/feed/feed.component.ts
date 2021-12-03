1914d256fd27537a8b96f30daaa98aafdb73c8e7
mszab0310 <mszabi0310@gmail.com>
2021-12-03 21:33:59 +0200

    Property binding for cards from feed and card service

--- news-website/src/app/feed/feed.component.ts
+++ news-website/src/app/feed/feed.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsResponse } from 'src/interfaces/NewsResponse';
+import { CardService } from './card.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Article } from 'src/interfaces/Article';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
 })
 export class FeedComponent implements OnInit {
+  public articles: Observable<Article[]> = this.cardService
+    .getEverything()
+    .pipe(
+      map((result: NewsResponse) => {
+        return result.articles;
+      })
+    );
 
-  constructor() { }
+  constructor(private cardService: CardService) {}
 
   ngOnInit(): void {
+    this.articles = this.cardService.getEverything().pipe(
+      map((result: NewsResponse) => {
+        return result.articles;
+      })
+    );
   }
-
 }
