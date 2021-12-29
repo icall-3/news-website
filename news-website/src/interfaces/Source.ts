26ceaf8d983659b30af2f269fb2cbd0d124e8780
dariabuciuman <buciuman.daria@gmail.com>
2021-12-29 19:36:06 +0200

    Add comments in source interface

--- news-website/src/interfaces/Source.ts
+++ news-website/src/interfaces/Source.ts
@@ -1,4 +1,14 @@
+/**
+ * Interface for the source of the article, used in the Article Interface
+ */
 export interface Source {
+  /**
+   * Identifier for the source
+   */
   id?: null;
+
+  /**
+   * The name of the source
+   */
   name: string;
 }
