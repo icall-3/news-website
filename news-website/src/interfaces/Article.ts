f0129c2f100f744d0c3dd300d974096d052caa7f
dariabuciuman <buciuman.daria@gmail.com>
2021-12-29 18:51:56 +0200

    Add comments in article interface

--- news-website/src/interfaces/Article.ts
+++ news-website/src/interfaces/Article.ts
@@ -1,12 +1,38 @@
 import { Source } from './Source';
-
+/**
+ * Interface for the article, used to categorize information
+ */
 export interface Article {
+  /**
+   * The source of the article
+   */
   source: Source;
+  /**
+   * The author of the article
+   */
   author?: null;
+  /**
+   * The title of the article
+   */
   title: string;
+  /**
+   * The description of the article
+   */
   description: string;
+  /**
+   * The url of the article
+   */
   url: string;
+  /**
+   * The url of the image of the article
+   */
   urlToImage: string;
+  /**
+   * The publishing date of the article
+   */
   publishedAt: string;
+  /**
+   * The description of the article
+   */
   content: string;
 }
