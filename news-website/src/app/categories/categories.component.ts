b7b330dc92558161b966169c4ef51933d3bdb82a
dariabuciuman <buciuman.daria@gmail.com>
2021-12-29 23:34:47 +0200

    Add comments in categories component

--- news-website/src/app/categories/categories.component.ts
+++ news-website/src/app/categories/categories.component.ts
@@ -1,16 +1,31 @@
 import { CardService } from './../feed/card.service';
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * The component that makes the navigation between categories possible
+ */
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  /**
+   * Includes the card service for requesting articles from the right category
+   * @param cardService The service that makes a request to the API
+   */
   constructor(private cardService: CardService) {}
 
+  /**
+   * @ignore
+   */
   ngOnInit(): void {}
 
+  /**
+   * Method to set the url by changing the category to the desired one
+   * and reloading the page with the new category
+   * @param category The category of the news
+   */
   public setUrl(category: string): void {
     var url =
       'https://newsapi.org/v2/top-headlines?country=us&category=' +
