f3f92c237f81ec8083d1a90f0c4657f20fbceff2
dariabuciuman <buciuman.daria@gmail.com>
2021-12-29 22:39:58 +0200

    Add comments in spinner service

--- news-website/src/app/spinner/spinner.service.ts
+++ news-website/src/app/spinner/spinner.service.ts
@@ -1,22 +1,40 @@
 import { flatten } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+/**
+ * The service that shows or hides the spinner
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class SpinnerService {
+  /**
+   * Observable to pass data about the spinner to other components
+   */
   private spinnerObs = new BehaviorSubject<boolean>(false);
+  /**
+   * @ignore
+   */
   constructor() {}
 
+  /**
+   * Method to pass data about the spinner to other components
+   * @returns The data about the spinner as an Observable
+   */
   public getSpinnerObs(): Observable<boolean> {
     return this.spinnerObs.asObservable();
   }
 
+  /**
+   * Sets the spinner as visible by setting the next value of the subject to true
+   */
   public show(): void {
     this.spinnerObs.next(true);
   }
 
+  /**
+   * Sets the spinner as hidden by setting the next value of the subject to false
+   */
   public hide(): void {
     this.spinnerObs.next(false);
   }
